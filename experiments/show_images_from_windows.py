68333694de9413f45878ab5d705a74ebeac6bc17
Matti1123 <fm9023@mci4me.at>
2026-02-13 15:45:50 +0100

    show_images_from_windows

---
+++ experiments/show_images_from_windows.py
@@ -0,0 +1,5 @@
+from PIL import Image
+import os
+
+img_dir = "/mnt/c/Users/flets/OneDrive/Documents/Uni allgemein/Bachelor_arbeit/erste_segmentierung/oxford_pets/images"
+print(os.listdir(img_dir)[:5])  # zeigt die ersten 5 Bilder
